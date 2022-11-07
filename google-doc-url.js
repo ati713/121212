28e61e67e116fc4109cd4607ce2a303858a08081
ati713 <117667068+ati713@users.noreply.github.com>
2022-11-07 17:25:34 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -3,4 +3,3 @@ var googleDocURL = https://docs.google.com/spreadsheets/d/e/2PACX-1vTajUi86itctf
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
-https://docs.google.com/spreadsheets/d/e/2PACX-1vTajUi86itctf6a5QXrRhblPDp7onrLgQ_7HEtK6eV1j3nV6bMjyjEuGDNEm5RSQPXDv3Xo0dwBhqm7/pubhtml
